487cdcaa93c2354a9d1de4e71dffc8b7e16199de
ToniaMera <77622398+ToniaMera@users.noreply.github.com>
2024-04-27 22:10:16 +0300

    Create train_model.py

---
+++ scripts/train_model.py
@@ -0,0 +1,36 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import graphenco as gr
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+data = pd.read_csv('train.csv')
+
+md = gr.mol_descriptors(data['Drug']).oned()
+
+
+tg = gr.ToGraph(data['Drug'], data['Y'], md).process()  
+train_Xy, test_Xy = train_test_split(tg, test_size=0.2, random_state=42)
+tm = gr.TrainModel(train_data = train_Xy, batch_sz = 24, epochs = 100, 
+                    model_name='graph_enc_model.pt', L = 'imbalance')
+tm.train()
+
+GE = gr.GraphEnc(test_Xy, 'graph_enc_model.pt')
+pr = GE.predict()
+
+true_values = []
+for data_point in test_Xy:
+    y_true = data_point.y
+    true_values.append(y_true)
+
+rmse = np.sqrt(mean_squared_error(true_values, pr))
+r2 = r2_score(true_values, pr)
+
+print(rmse, r2)
+
+plt.scatter(pr, true_values, s = 20, alpha = 0.5)
+plt.plot([-1, 4], [-1, 4], label = 'x = y', c = 'blue', alpha = 0.5)
+plt.xlabel('Predicted logP')
+plt.ylabel('True LogP')
