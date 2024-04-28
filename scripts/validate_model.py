57c3ed77388390f40caa36d7b515081b4d0c413e
ToniaMera <77622398+ToniaMera@users.noreply.github.com>
2024-04-28 18:54:26 +0300

    Update validate_model.py

--- scripts/validate_model.py
+++ scripts/validate_model.py
@@ -16,14 +16,14 @@ tg = gr.ToGraph(data['Drug'], data['Y']).process()
 
 train_Xy, test_XY = train_test_split(tg, test_size=0.4, random_state=42)
 val_Xy, test_Xy = train_test_split(test_XY, test_size=0.5, random_state=42)
-tm = gr.TrainModel(train_data = train_Xy, batch_sz = 24, epochs = 200, Xy_eval = val_Xy)
-g1, g2 = tm.train()
+tm1 = gr.TrainModel(train_data = train_Xy, batch_sz = 24, epochs = 200, Xy_eval = val_Xy)
+g1, g2 = tm1.train()
 
 tg = gr.ToGraph(data['Drug'], data['Y'], md).process()   
 train_Xy, test_XY = train_test_split(tg, test_size=0.4, random_state=42)
 val_Xy, test_Xy = train_test_split(test_XY, test_size=0.5, random_state=42)
-tm = gr.TrainModel(train_data = train_Xy, batch_sz = 24, epochs = 200, Xy_eval = val_Xy)
-e1, e2 = tm.train()
+tm2 = gr.TrainModel(train_data = train_Xy, batch_sz = 24, epochs = 200, Xy_eval = val_Xy)
+e1, e2 = tm2.train()
 
 with torch.no_grad():
     plt.plot(g1, label = 'Graph Training Loss')
