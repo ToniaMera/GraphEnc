ca1261f527aab71867f05c8a36c4bd9feb5d542b
ToniaMera <77622398+ToniaMera@users.noreply.github.com>
2024-05-02 16:58:05 +0300

    Update graphenc.py

--- graph_encoder_module/graphenc.py
+++ graph_encoder_module/graphenc.py
@@ -29,11 +29,11 @@ class ToGraph(Dataset):
             ei = self.edge_features(mol)
             if self.MD is not None and self.y is not None:
                 md = self.MD[c]
-                data = Data(x = nf, edge_index = ei, y = self.y[c], md = md)
+                data = Data(x = nf, edge_index = ei, y = self.y[c], md = self.MD)
             elif self.MD is None and self.y is not None:
                 data = Data(x = nf, edge_index = ei, y = self.y[c])
             elif self.MD is not None and self.y is None:
-                data = Data(x = nf, edge_index = ei, md = md)
+                data = Data(x = nf, edge_index = ei, md = self.MD)
             else:
                 data = Data(x = nf, edge_index = ei)
                 
